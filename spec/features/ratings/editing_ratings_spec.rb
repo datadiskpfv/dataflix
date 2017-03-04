d1eb7b68d19442da1f6f2e557387cf8b99f183fb
Paul Valle <paul.valle@datadisk.co.uk>
2017-03-04 20:58:20 +0000

    added rating to films, making a start on first film image upload using carrierwave - pfv

--- spec/features/ratings/editing_ratings_spec.rb
+++ spec/features/ratings/editing_ratings_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.feature ' Users can edit ratings' do
 
   scenario 'with valid credentials' do
-    rating = FactoryGirl.create(:rating, rating: '18')
-
     visit '/ratings'
     click_link "edit_18"
 
