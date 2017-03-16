58d460011065b2037e45682f989796000f1641d4
Paul Valle <paul.valle@datadisk.co.uk>
2017-03-16 14:57:19 +0000

    performed some tidying up regarding spec tests - pfv

--- spec/features/films/table_films_spec.rb
+++ spec/features/films/table_films_spec.rb
@@ -14,21 +14,21 @@ RSpec.feature 'Test films table view' do
     puts "Film2 image: #{film2.image1}"
   end
 
-  scenario 'list action only movies', js: true do
+  scenario 'list action only movies' do
     within('.grey_box') { click_link 'Action' }
 
-    expect(page).to have_content 'Films (table)'
+    expect(page).to have_content 'Films (Action)'
     expect(page).to have_content 'Black Mass'
     expect(page).not_to have_content 'Alien'
-    within('.film_table') { expect(page).to have_css("img[src*='black-mass']") }
+    within('.table-paginate') { expect(page).to have_css("img[src*='black-mass']") }
   end
 
   scenario 'list horror only movies' do
     click_link 'Horror'
-    expect(page).to have_content 'Films (table)'
+    expect(page).to have_content 'Films (Horror)'
     expect(page).to have_content 'Alien'
     expect(page).not_to have_content 'Black Mass'
-    within('.film_table') { expect(page).to have_css("img[src*='alien']") }
+    within('.table-paginate') { expect(page).to have_css("img[src*='alien']") }
   end
 
 end
