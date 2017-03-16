4287d9c162bf4ffcf88e440c10762d9c26438f00
Paul Valle <paul.valle@datadisk.co.uk>
2017-03-16 13:53:28 +0000

    performed some tidying up regarding spec tests - pfv

--- spec/controllers/counties_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CountiesController, type: :controller do
-
-end
--- spec/controllers/postcodes_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PostcodesController, type: :controller do
-
-end
