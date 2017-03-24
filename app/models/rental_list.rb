53c3f8f93f61a86ea7b8de2c45ef793ddea7550e
Paul Valle <paul.valle@datadisk.co.uk>
2017-03-24 14:58:37 +0000

    back on the rental list screen, add a coffeescript to remove table row when deleting a film thus no need to re-render whole page - pfv

--- app/models/rental_list.rb
+++ app/models/rental_list.rb
@@ -3,5 +3,5 @@ class RentalList < ActiveRecord::Base
   belongs_to :user
   belongs_to :film
 
-  validates_uniqueness_of :film_id, :scope => :film_format, :scope => :user_id
+  validates_uniqueness_of :film_id, scope: [ :film_format, :user_id ]
 end
