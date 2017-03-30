0f700137eaa3adf6c416d84e85610b0b47a0d0ad
Paul Valle <paul.valle@datadisk.co.uk>
2017-03-30 13:18:15 +0100

    updated get_user_name helper to handle emails address with dots or no dots - pfv

--- app/helpers/dataflix/settings_helper.rb
+++ app/helpers/dataflix/settings_helper.rb
@@ -2,8 +2,8 @@ module Dataflix::SettingsHelper
 
   def get_user_name(user)
     user_name = user.email.split("@").first
-    first_name = user_name.split('.').first.capitalize
-    last_name = user_name.split('.').last.capitalize
-    return "#{first_name} #{last_name}"
+    name = user_name.split('.')
+
+    return "#{name.map(&:capitalize).join(' ')}"
   end
 end
